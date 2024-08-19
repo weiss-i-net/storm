858d62b20e38ea59a0977334a475ff6bdec648db
Jannik Hiller <jannik.hiller@live.de>
2024-08-19 11:09:15 +0200

    made helper class similar to other modelcheckers.

---
+++ src/storm/modelchecker/rpatl/helper/SparseSmgRpatlHelper.cpp
@@ -0,0 +1,103 @@
+//
+// Created by janni on 30/07/2024.
+//
+
+#include "storm/modelchecker/rpatl/helper/SparseSmgRpatlHelper.h"
+
+#include <boost/format.hpp>
+
+#include "exceptions/NotSupportedException.h"
+#include "storage/BitVector.h"
+#include "storage/SparseMatrix.h"
+
+namespace storm::modelchecker::helper {
+
+template<typename ValueType, typename SolutionType>
+storm::storage::BitVector storm::modelchecker::helper::SparseSmgRpatlHelper<ValueType, SolutionType>::computeStrongAttractors(
+    storm::storage::BitVector const& maximizerCoalition, storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+    storm::storage::SparseMatrix<ValueType> const& backwardTransitions, storm::storage::BitVector const& targetStateSet,
+    storm::storage::BitVector const& allowedStateSet) {
+    if constexpr (std::is_same_v<ValueType, storm::Interval> or std::is_same_v<ValueType, RationalNumber>) {
+        STORM_LOG_THROW(false, storm::exceptions::NotSupportedException, "Intervals and RationalNumbers not supported for SMGs.");
+    } else {
+        STORM_LOG_ERROR(boost::format("maximizer: %1%, target: %2%, allowed: %3%") % maximizerCoalition.size() % targetStateSet.size() % allowedStateSet.size());
+        auto stateSet{targetStateSet};
+        std::vector stack(targetStateSet.begin(), targetStateSet.end());
+
+        while (not stack.empty()) {
+            const auto currentState = stack.back();
+            stack.pop_back();
+
+            for (const auto& predecessorEntry : backwardTransitions.getRow(currentState)) {
+                const auto predecessor = predecessorEntry.getColumn();
+                if (not allowedStateSet.get(predecessor))
+                    break;
+                if (stateSet.get(predecessor))
+                    break;
+
+                const auto nonDeterministicChoices = transitionMatrix.getRowGroupIndices(predecessor);
+
+                const auto leadsToCurrentStateSet = [&](const auto& row_index) {
+                    // it's enough when one of the successors is in stateSet as we tread the 'nature' states as belonging to the maximizer
+                    const auto& transition_row = transitionMatrix.getRow(row_index);
+                    return std::any_of(transition_row.begin(), transition_row.end(), [&](auto entry) { return stateSet.get(entry.getColumn()); });
+                };
+
+                // if the predecessor belongs to the maximizer one choice leading to currentStateSet is enough
+                // if not, all need to lead to stateSet
+                const bool predecessorIsValid = maximizerCoalition.get(predecessor)
+                                                    ? std::any_of(nonDeterministicChoices.begin(), nonDeterministicChoices.end(), leadsToCurrentStateSet)
+                                                    : std::all_of(nonDeterministicChoices.begin(), nonDeterministicChoices.end(), leadsToCurrentStateSet);
+
+                if (predecessorIsValid) {
+                    stateSet.set(predecessor, true);
+                    stack.push_back(predecessor);
+                }
+            }
+        }
+        return stateSet;
+    }
+}
+template<typename ValueType, typename SolutionType>
+storm::storage::BitVector storm::modelchecker::helper::SparseSmgRpatlHelper<ValueType, SolutionType>::computeWeakAttractors(
+    storm::storage::BitVector const& maximizerCoalition, storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+    storm::storage::SparseMatrix<ValueType> const& backwardTransitions, storm::storage::BitVector const& targetStateSet,
+    storm::storage::BitVector const& allowedStateSet) {
+    auto currentStateSet{targetStateSet};
+    auto nextStateSet = computeStrongAttractors(maximizerCoalition, transitionMatrix, backwardTransitions, targetStateSet, allowedStateSet);
+
+    while (currentStateSet != nextStateSet) {
+        currentStateSet = std::move(nextStateSet);
+        auto badStateSet =
+            computeStrongAttractors(
+                ~maximizerCoalition,
+                transitionMatrix,
+                backwardTransitions,
+                ~currentStateSet,
+                allowedStateSet & ~targetStateSet);
+        nextStateSet = computeStrongAttractors(maximizerCoalition, transitionMatrix, backwardTransitions, targetStateSet, allowedStateSet & ~badStateSet);
+    }
+    return currentStateSet;
+}
+
+template<typename ValueType, typename SolutionType>
+storm::storage::BitVector storm::modelchecker::helper::SparseSmgRpatlHelper<ValueType, SolutionType>::computeUntilProp1(
+    storm::storage::BitVector const& maximizerCoalition, storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+    storm::storage::SparseMatrix<ValueType> const& backwardTransitions, storm::storage::BitVector const& phiStates,
+    storm::storage::BitVector const& psiStates) {
+    return computeWeakAttractors(maximizerCoalition, transitionMatrix, backwardTransitions, psiStates, phiStates);
+}
+
+template<typename ValueType, typename SolutionType>
+storm::storage::BitVector storm::modelchecker::helper::SparseSmgRpatlHelper<ValueType, SolutionType>::computeUntilProp0(
+    storm::storage::BitVector const& maximizerCoalition, storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+    storm::storage::SparseMatrix<ValueType> const& backwardTransitions, storm::storage::BitVector const& phiStates,
+    storm::storage::BitVector const& psiStates) {
+    return ~computeStrongAttractors(maximizerCoalition, transitionMatrix, backwardTransitions, psiStates, phiStates);;
+}
+
+template class SparseSmgRpatlHelper<double>;
+template class SparseSmgRpatlHelper<storm::Interval, double>;
+template class SparseSmgRpatlHelper<storm::RationalNumber>;
+
+}  // namespace storm::modelchecker::helper
--- src/storm/modelchecker/rpatl/helper/SparseSmgRpatlHelper.h
+++ src/storm/modelchecker/rpatl/helper/SparseSmgRpatlHelper.h
@@ -8,63 +8,35 @@
 // Created by Jannik Hiller on 16/08/2023.
 //
 
-#include <vector>
 #include "solver/SolveGoal.h"
 #include "storage/BitVector.h"
 
-template<typename ValueType>
-storm::storage::BitVector computeStrongAttractors(storm::storage::BitVector const& maximizerCoalition,
-                                                  storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
-                                                  storm::storage::SparseMatrix<ValueType> const& backwardTransitions,
-                                                  storm::storage::BitVector const& targetStates, storm::storage::BitVector const& allowedStates) {
-    auto stateSet{targetStates};
-    std::vector<uint_fast64_t> stack{targetStates.begin(), targetStates.end()};
-    while (not stack.empty()) {
-        const auto currentState = stack.back();
-        stack.pop_back();
+namespace storm::modelchecker::helper {
 
-        for (const auto& predecessorEntry : backwardTransitions.getRow(currentState)) {
-            const auto predecessor = predecessorEntry.getColumn();
-            if (not allowedStates.get(predecessor))
-                break;
-            if (stateSet.get(predecessor))
-                break;
+template<typename ValueType, typename SolutionType = ValueType>
+class SparseSmgRpatlHelper {
+   private:
+    static storm::storage::BitVector computeStrongAttractors(storm::storage::BitVector const& maximizerCoalition,
+                                                             storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+                                                             storm::storage::SparseMatrix<ValueType> const& backwardTransitions,
+                                                             storm::storage::BitVector const& targetStateSet, storm::storage::BitVector const& allowedStateSet);
 
-            const auto nonDeterministicChoices = transitionMatrix.getRowGroup(predecessor);
-            const auto leadsToCurrentStateSet = [&](const auto& transition_row) {
-                // it's enough when one of the successors is in stateSet as we tread the 'nature' states as belonging to the maximizer
-                return std::any_of(transition_row.begin(), transition_row.end(), [&](auto entry) { return stateSet.get(entry.getColumn()); });
-            };
+    static storm::storage::BitVector computeWeakAttractors(storm::storage::BitVector const& maximizerCoalition,
+                                                           storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+                                                           storm::storage::SparseMatrix<ValueType> const& backwardTransitions,
+                                                           storm::storage::BitVector const& targetStateSet, storm::storage::BitVector const& allowedStateSet);
 
-            // if the predecessor belongs to the maximizer one choice leading to currentStateSet is enough
-            // if not, all need to lead to stateSet
-            const bool predecessorIsValid = maximizerCoalition.get(predecessor)
-                                                ? std::any_of(nonDeterministicChoices.begin(), nonDeterministicChoices.end(), leadsToCurrentStateSet)
-                                                : std::all_of(nonDeterministicChoices.begin(), nonDeterministicChoices.end(), leadsToCurrentStateSet);
+   public:
+    static storm::storage::BitVector computeUntilProp1(storm::storage::BitVector const& maximizerCoalition,
+                                                       storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+                                                       storm::storage::SparseMatrix<ValueType> const& backwardTransitions,
+                                                       storm::storage::BitVector const& phiStates, storm::storage::BitVector const& psiStates);
+    static storm::storage::BitVector computeUntilProp0(storm::storage::BitVector const& maximizerCoalition,
+                                                       storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
+                                                       storm::storage::SparseMatrix<ValueType> const& backwardTransitions,
+                                                       storm::storage::BitVector const& phiStates, storm::storage::BitVector const& psiStates);
+};
 
-            if (predecessorIsValid) {
-                stateSet.set(predecessor, true);
-                stack.push_back(predecessor);
-            }
-        }
-    }
-    return stateSet;
-}
-
-template<typename ValueType>
-auto computeWeakAttractors(storm::storage::BitVector const& maximizerCoalition, storm::storage::SparseMatrix<ValueType> const& transitionMatrix,
-                           storm::storage::SparseMatrix<ValueType> const& backwardTransitions, storm::storage::BitVector const& targetStateSet,
-                           storm::storage::BitVector const& allowedStateSet) -> storm::storage::BitVector {
-    auto currentStateSet{targetStateSet};
-    auto nextStateSet = computeStrongAttractors(maximizerCoalition, transitionMatrix, backwardTransitions, targetStateSet, allowedStateSet);
-
-    while (currentStateSet != nextStateSet) {
-        currentStateSet = std::move(nextStateSet);
-        auto badStateSet =
-            computeStrongAttractors(~maximizerCoalition, transitionMatrix, backwardTransitions, ~currentStateSet, allowedStateSet % ~targetStateSet);
-        nextStateSet = computeStrongAttractors(maximizerCoalition, transitionMatrix, backwardTransitions, targetStateSet, allowedStateSet % ~badStateSet);
-    }
-    return currentStateSet;
-}
+}  // namespace storm::modelchecker::helper
 
 #endif  // STORM_SPARSESMGRPATLHELPER_H
